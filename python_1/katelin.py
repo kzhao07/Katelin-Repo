f5663d6e2d03d6466c4ac16b46d520444d809718
SA23 <sa23@SA23s-MacBook-Air.local>
2024-11-06 18:10:37 -0500

    I made my algorithm and flowchart

---
+++ python_1/katelin.py
@@ -0,0 +1,23 @@
+# Snake Case
+first_name = "Katelin" # this is a string
+
+#Camel case
+lastName = "Zhao" # this is also a string
+nickName = "Kate" # this is a string
+
+age = 16 # this is an int
+bankAccountBalance = 10501923.56 # this is a float
+
+print(first_name)
+print(bankAccountBalance)
+
+if bankAccountBalance > 500:
+    print("balla")
+else:
+    print("not balla")
+
+print("Welcome to the APPLE SAUCE PROGRAM")
+
+numOfUserAppleSauce = input("How many apple sauces do you like: ")
+
+print("The user said", numOfUserAppleSauce)
