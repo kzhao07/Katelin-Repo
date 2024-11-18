560b251ee2165d03e7824620bb41cd443e310fe4
SA23 <sa23@SA23s-MacBook-Air.local>
2024-11-18 17:59:06 -0500

    Worked on Python project

--- python_1/project.py
+++ python_1/project.py
@@ -18,6 +18,8 @@ def displayCards(): # display the cards in a 3x4 format
     for row in table:
         print(" ".join(card.printSymbol() for card in row))
 
+    print()
+
 def generateCards(): # generate the pairs of numbers to assign to cards and create a deck of cards
     global deck # globalize deck
     global pairs # globalize pairs
@@ -29,20 +31,50 @@ def generateCards(): # generate the pairs of numbers to assign to cards and crea
     for i in range(12):
         deck.append(Card(pairs[i])) # create a deck of cards with values
 
-    np.random.shufle(deck)
+    np.random.shuffle(deck) # shuffle the order of the cards in the deck
     
     pos = 0 # keep track of position in deck
     for rows in range(3):
         tempArray = []
         for cols in range(4):
             tempArray.append(deck[pos])
-            pos = pos+1
+            pos += 1
         table.append(tempArray)
     
     # create a deck of cards in a 3x4 format
 
-
+def allFlipped():
+    for rows in range(3):
+        for cols in range(4):
+            if table[rows][cols].faceUp == False:
+                return False
+    return True
 def main(): # main function
     generateCards() 
     displayCards()
+    prevValue = -1
+    prevRow = -1
+    prevCol = -1
+    print("Welcome to Matching Madness! Enter the row and column of the card you would like to flip.")
+    while(allFlipped() == False):
+        userRow = int(input("Enter the row: "))-1
+        userCol = int(input("Enter the column: "))-1
+        if (userRow >= 3 or userRow < 0 or userCol < 0 or userCol >= 4):
+            print("Invalid input. Please try again")
+        else:
+            currentVal = table[userRow][userCol].value
+            table[userRow][userCol].faceUp = True
+            if prevValue == currentVal:
+                displayCards()
+                table[prevRow][prevCol].faceUp = True
+            elif prevValue != -1:
+                displayCards()
+                table[userRow][userCol].faceUp = False
+                table[prevRow][prevCol].faceUp = False
+            else: 
+                displayCards()
+            prevValue = currentVal
+            prevRow = userRow
+            prevCol = userCol
+    print("Game Over")
 main()
